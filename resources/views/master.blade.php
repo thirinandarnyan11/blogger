97e0876cd0c8fb53e666915bd4d32eca49cba30d
Thet Thet Naing(Mi Naing) <thetthetnaing@ubuntu.ubuntu-domain>
2020-06-30 16:00:00 +0630

    hello master blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>hrllo</title>
 
   <!-- Custom fonts for this template-->
   <link href="{{asset('backendtemplate/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
